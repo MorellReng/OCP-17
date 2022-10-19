5295656ca46c484f2d3f6350b1f36b50b0e361d1
Andreas Morell-Reng <arm@commentor.dk>
2022-10-19 16:02:37 +0200

    Chap4 - Working with Durations

--- test/chap4/time/DurationTest.java
+++ test/chap4/time/DurationTest.java
@@ -1,11 +1,35 @@
 package chap4.time;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.UnsupportedTemporalTypeException;
 
 public class DurationTest {
 
     @Test
-    void testing() {
+    void toStringDaily() {
+        Duration daily = Duration.ofDays(1);
+        Duration daily2 = Duration.of(1, ChronoUnit.DAYS);
 
+        String expected = "PT24H";
+
+        Assertions.assertEquals(expected, daily.toString());
+        Assertions.assertEquals(expected, daily2.toString());
+    }
+
+    @Test
+    void durationOfChronUnitTwoYears() {
+        // throws exception
+        // Duration twoYears = Duration.of(2, ChronoUnit.YEARS);
+        Assertions.assertThrows(UnsupportedTemporalTypeException.class, () -> Duration.of(2, ChronoUnit.YEARS));
+
+
+        // 24 * 365 * 2
+        String expected = "PT17520H";
+
+        // Assertions.assertEquals(expected, twoYears.toString());
     }
 }
